9ca5b25ea1e68f97c76248919248332565db4a06
wohu8292 <108155439+wohu8292@users.noreply.github.com>
2022-11-03 13:03:06 -0600

    hw11

---
+++ Assignment/Weekly_Assignment/assignment_11/hw11.js
@@ -0,0 +1,28 @@
+const Generate=document.querySelector(".generate");
+const Quote=document.querySelector(".quote");
+const Author=document.querySelector(".author");
+const Imagee=document.querySelector(".imagee");
+const Wrapper=document.querySelector(".wrapper");
+
+Generate.addEventListener('click', getQuote);
+const api_url='https://thesimpsonsquoteapi.glitch.me/quotes?count=num';
+const img = document.createElement("img");
+img.style.height='300px';
+
+async function getQuote(){
+    console.log("hello");
+    let Quote=await fetch(api_url);
+    let response=await Quote.text();
+    console.log(response);
+    let json_par=JSON.parse(response);
+    console.log(json_par[0].quote);
+    displayQuote(json_par[0].quote, json_par[0].image);
+}
+
+function displayQuote(x,y){
+    img.src = y;
+    Wrapper.appendChild(img);
+    Quote.textContent=x;
+}
+
+getQuote();
